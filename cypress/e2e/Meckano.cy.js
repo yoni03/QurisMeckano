cd9c15f282277c2f17106a35f0e0b4a052b238ae
yoni03 <yoni03@gmail.com>
2023-03-05 11:40:57 +0200

    Update Meckano.cy.js

--- cypress/e2e/Meckano.cy.js
+++ cypress/e2e/Meckano.cy.js
@@ -6,9 +6,9 @@ it('Meckano', function() {
   cy.get('#password').type('zu9asy4u');
   cy.wait(500);
   cy.get('#submitButtons > .sl').click();
-  cy.wait(500);
-  cy.get('#checkin-button').click();
   cy.wait(1000);
+  cy.get('#checkin-button').click();
+  cy.wait(4000);
   cy.get('.select2-selection__rendered').last().click();
   cy.get('li.select2-results__option').first().click();
   cy.get('.btn-rounded-blue').click();
