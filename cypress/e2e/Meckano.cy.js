4b7d397d48f51cf756d81eb15901777e6e4f2dfe
yoni03 <yoni03@gmail.com>
2023-03-05 11:48:29 +0200

    Update Meckano.cy.js

--- cypress/e2e/Meckano.cy.js
+++ cypress/e2e/Meckano.cy.js
@@ -1,6 +1,4 @@
-/* ==== Test Created with Cypress Studio ==== */
 it('Meckano', function() {
-  /* ==== Generated with Cypress Studio ==== */
   cy.visit('https://app.meckano.co.il/login.php');
   cy.get('#email').type('yoni@quris.ai');
   cy.get('#password').type('zu9asy4u');
@@ -8,9 +6,4 @@ it('Meckano', function() {
   cy.get('#submitButtons > .sl').click();
   cy.wait(1000);
   cy.get('#checkin-button').click();
-  cy.wait(4000);
-  cy.get('.select2-selection__rendered').last().click();
-  cy.get('li.select2-results__option').first().click();
-  cy.get('.btn-rounded-blue').click();
-    /* ==== End Cypress Studio ==== */
 });
